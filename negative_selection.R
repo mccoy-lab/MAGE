8acbde53c2156587f9666f072dc76f8592cabf63
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2023-07-21 15:08:43 -0400

    Create negative_selection.R

---
+++ negative_selection.R
@@ -0,0 +1,285 @@
+library(data.table)
+library(tidyverse)
+library(readxl)
+library(broom)
+library(cowplot)
+
+dt <- fread("~/Downloads/eQTL_finemapping.genes.txt.gz") %>%
+  setnames(., "geneID", "gene") %>%
+  .[, gene := gsub("\\..*", "", gene)]
+
+disp <- fread("~/Downloads/dispersions.csv")[expression_threshold.autosome == TRUE] %>%
+  .[, gene := gsub("\\..*", "", gene)]
+
+disp <- merge(dt, disp[expression_threshold.autosome == TRUE, c("gene", "baseMean", "dispersion")])
+  
+plof <- fread("bgzip -c -d https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_gene.txt.bgz") %>%
+  setnames(., "gene", "gene_symbol") %>%
+  setnames(., "gene_id", "gene")
+
+disp <- merge(disp, plof, by = "gene", all.x = TRUE)
+
+ds <- fread("~/Downloads/Collins_rCNV_2022.dosage_sensitivity_scores.tsv.gz") %>%
+  setnames(., c("gene_symbol", "pHaplo", "pTriplo"))
+
+symbol2name <- fread("~/Downloads/gene_names.txt") %>%
+  setnames(., c("gene", "gene_full", "gene_symbol"))
+
+ds <- merge(ds, symbol2name, by = "gene_symbol")
+
+disp <- merge(disp, ds, by = "gene", all.x = TRUE)
+
+tx2gene <- fread("~/Downloads/mart_export.txt")[, c(1, 3)] %>%
+  setnames(., c("gene", "Ensembl_transcript_id"))
+
+hs <- fread("https://raw.githubusercontent.com/agarwal-i/loss-of-function-fitness-effects/main/out/Supplementary%20File%202.txt") %>%
+  setorder(., -log10_map) %>%
+  setnames(., "Gene", "gene_symbol") %>%
+  merge(tx2gene, by = "Ensembl_transcript_id") %>%
+  setnames(., "log10_map", "hs") %>%
+  .[, hs := 10^hs]
+
+disp <- merge(disp, hs[, c("gene", "hs")], by = "gene", all.x = TRUE)
+
+rvis <- read_xlsx("~/Downloads/pgen.1003709.s002.xlsx") %>%
+  as.data.table() %>%
+  setnames(., c("gene_symbol", "rvis", "rvis_percentile"))
+rvis <- merge(rvis, symbol2name, by = "gene_symbol")[, c("gene", "rvis")]
+
+disp <- merge(disp, rvis, by = "gene", all.x = TRUE)
+
+disp <- disp %>%
+  mutate(., expression_decile = ntile(baseMean, 10)) %>%
+  mutate(., hs_decile = ntile(hs, 10)) %>%
+  mutate(., rvis_decile = ntile(rvis, 10)) %>%
+  mutate(., loeuf_decile = ntile(oe_lof_upper, 10)) %>%
+  mutate(., pLI_decile = ntile(pLI, 10)) %>%
+  mutate(., pHaplo_decile = ntile(pHaplo, 10)) %>%
+  mutate(., pTriplo_decile = ntile(pTriplo, 10)) %>%
+  as.data.table()
+
+loeuf_dt <- group_by(disp[!is.na(loeuf_decile)], numCredibleSets, loeuf_decile == 1) %>%
+  summarize(., n = n()) %>%
+  as.data.table() %>%
+  setnames(., c("numCredibleSets", "constraint_criterion", "n")) %>%
+  .[!(is.na(constraint_criterion))]
+loeuf_dt[, density := as.numeric(NA)]
+loeuf_dt[constraint_criterion == TRUE, density := n / sum(n)]
+loeuf_dt[constraint_criterion == FALSE, density := n / sum(n)]
+loeuf_dt[, metric := "LOEUF"]
+
+loeuf_p <- glm(data = disp[!is.na(loeuf_decile)], formula = numCredibleSets ~ baseMean + (loeuf_decile == 1), family = "quasipoisson") %>%
+  tidy() %>%
+  as.data.table() %>%
+  .[3,] %>%
+  .$p.value
+
+pli_dt <- group_by(disp[!is.na(pLI_decile)], numCredibleSets, pLI_decile == 10) %>%
+  summarize(., n = n()) %>%
+  as.data.table() %>%
+  setnames(., c("numCredibleSets", "constraint_criterion", "n")) %>%
+  .[!(is.na(constraint_criterion))]
+pli_dt[, density := as.numeric(NA)]
+pli_dt[constraint_criterion == TRUE, density := n / sum(n)]
+pli_dt[constraint_criterion == FALSE, density := n / sum(n)]
+pli_dt[, metric := "pLI"]
+
+pli_p <- glm(data = disp[!is.na(pLI_decile)], formula = numCredibleSets ~ baseMean + (pLI_decile == 10), family = "quasipoisson") %>%
+  tidy() %>%
+  as.data.table() %>%
+  .[3,] %>%
+  .$p.value
+
+pHaplo_dt <- group_by(disp[!is.na(pHaplo_decile)], numCredibleSets, pHaplo_decile == 10) %>%
+  summarize(., n = n()) %>%
+  as.data.table() %>%
+  setnames(., c("numCredibleSets", "constraint_criterion", "n")) %>%
+  .[!(is.na(constraint_criterion))]
+pHaplo_dt[, density := as.numeric(NA)]
+pHaplo_dt[constraint_criterion == TRUE, density := n / sum(n)]
+pHaplo_dt[constraint_criterion == FALSE, density := n / sum(n)]
+pHaplo_dt[, metric := "pHaplo"]
+
+pHaplo_p <- glm(data = disp[!is.na(pHaplo_decile)], formula = numCredibleSets ~ baseMean + (pHaplo_decile == 10), family = "quasipoisson") %>%
+  tidy() %>%
+  as.data.table() %>%
+  .[3,] %>%
+  .$p.value
+
+pTriplo_dt <- group_by(disp[!is.na(pTriplo_decile)], numCredibleSets, pTriplo_decile == 10) %>%
+  summarize(., n = n()) %>%
+  as.data.table() %>%
+  setnames(., c("numCredibleSets", "constraint_criterion", "n")) %>%
+  .[!(is.na(constraint_criterion))]
+pTriplo_dt[, density := as.numeric(NA)]
+pTriplo_dt[constraint_criterion == TRUE, density := n / sum(n)]
+pTriplo_dt[constraint_criterion == FALSE, density := n / sum(n)]
+pTriplo_dt[, metric := "pTriplo"]
+
+pTriplo_p <- glm(data = disp[!is.na(pTriplo_decile)], formula = numCredibleSets ~ (pTriplo_decile == 10), family = "quasipoisson") %>%
+  tidy() %>%
+  as.data.table() %>%
+  .[2,] %>%
+  .$p.value
+
+hs_dt <- group_by(disp[!is.na(hs_decile)], numCredibleSets, hs_decile == 10) %>%
+  summarize(., n = n()) %>%
+  as.data.table() %>%
+  setnames(., c("numCredibleSets", "constraint_criterion", "n")) %>%
+  .[!(is.na(constraint_criterion))]
+hs_dt[, density := as.numeric(NA)]
+hs_dt[constraint_criterion == TRUE, density := n / sum(n)]
+hs_dt[constraint_criterion == FALSE, density := n / sum(n)]
+hs_dt[, metric := "hs"]
+
+hs_p <- glm(data = disp[!is.na(hs_decile)], formula = numCredibleSets ~ (hs_decile == 10), family = "quasipoisson") %>%
+  tidy() %>%
+  as.data.table() %>%
+  .[2,] %>%
+  .$p.value
+
+rvis_dt <- group_by(disp[!is.na(rvis_decile)], numCredibleSets, rvis_decile == 1) %>%
+  summarize(., n = n()) %>%
+  as.data.table() %>%
+  setnames(., c("numCredibleSets", "constraint_criterion", "n")) %>%
+  .[!(is.na(constraint_criterion))]
+rvis_dt[, density := as.numeric(NA)]
+rvis_dt[constraint_criterion == TRUE, density := n / sum(n)]
+rvis_dt[constraint_criterion == FALSE, density := n / sum(n)]
+rvis_dt[, metric := "RVIS"]
+
+rvis_p <- glm(data = disp[!is.na(rvis_decile)], formula = numCredibleSets ~ (rvis_decile == 1), family = "quasipoisson") %>%
+  tidy() %>%
+  as.data.table() %>%
+  .[2,] %>%
+  .$p.value
+
+constraint_dt <- rbind(pli_dt, loeuf_dt, pHaplo_dt, pTriplo_dt, hs_dt, rvis_dt)
+constraint_dt[, density_flip := density]
+constraint_dt[constraint_criterion == FALSE, density_flip := -1 * density]
+constraint_dt[, y_facet := as.character(NA)]
+constraint_dt[constraint_criterion == TRUE, y_facet := "Top 10%"]
+constraint_dt[constraint_criterion == FALSE, y_facet := "Lower 90%"]
+
+constraint_dt$y_facet <- factor(constraint_dt$y_facet, levels = c("Top 10%", "Lower 90%"))
+
+ggplot(data = constraint_dt, aes(x = numCredibleSets, y = density, color = y_facet)) +
+  geom_point() +
+  geom_line() +
+  xlim(-1, 11) +
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank()) +
+  facet_grid(. ~ metric, scales = "free") + 
+  ylab("Density") +
+  xlab("Number of credible sets") +
+  scale_color_brewer(palette = "Set2", name = "")
+
+ggplot(data = constraint_dt, aes(x = numCredibleSets, y = density_flip, fill = y_facet)) +
+  geom_bar(stat = "identity") +
+  xlim(-1, 11) +
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank(), legend.position = "none") +
+  facet_grid(y_facet ~ metric, scales = "free") + 
+  scale_y_continuous(labels = abs) +
+  ylab("Density") +
+  xlab("Number of credible sets") +
+  scale_fill_brewer(palette = "Set2")
+
+constraint_dt$metric <- factor(constraint_dt$metric, 
+                               levels = c("pLI", "LOEUF", "pHaplo", "pTriplo", "hs", "RVIS"))
+
+
+pval_dt <- data.table(metric = c("pLI", "LOEUF", "pHaplo", "pTriplo", "hs", "RVIS"),
+           pval = c(pli_p, loeuf_p, pHaplo_p, pTriplo_p, hs_p, rvis_p),
+           y_facet = "Top 10%")
+
+pval_dt$metric <- factor(pval_dt$metric, 
+                         levels = c("pLI", "LOEUF", "pHaplo", "pTriplo", "hs", "RVIS"))
+
+top_panel <- ggplot(data = constraint_dt, aes(x = numCredibleSets, y = density, color = y_facet)) +
+  geom_point(size = 1) +
+  geom_line() +
+  xlim(-0.2, 11) +
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank()) +
+  facet_grid(. ~ metric, scales = "free") + 
+  ylab("Density") +
+  xlab("Number of credible sets") +
+  scale_color_brewer(palette = "Set2", name = "") +
+  geom_text(data = pval_dt, aes(x = 7, y = 0.35, 
+                                label = paste("p =", formatC(pval, format = "e", digits = 2))), 
+            color = "black", size = 3)
+
+####
+
+bestHits <- fread("~/Downloads/eQTL_finemapping.bestHits.txt.gz") %>%
+  setnames(., "geneID", "gene_id") %>%
+  setnames(., "variantID", "variant_id")
+
+fastQTL <- fread("~/Downloads/fastqtl_all.significantPairs.txt.gz")
+
+bestHits <- merge(bestHits, fastQTL, by = c("gene_id", "variant_id")) %>%
+  .[, gene_id := gsub("\\..*", "", gene_id)]
+
+rm(fastQTL)
+
+bestHits[, pLI := gene_id %in% disp[pLI_decile == 10]$gene]
+bestHits[, LOEUF := gene_id %in% disp[loeuf_decile == 1]$gene]
+bestHits[, pHaplo := gene_id %in% disp[pHaplo_decile == 10]$gene]
+bestHits[, pTriplo := gene_id %in% disp[pTriplo_decile == 10]$gene]
+bestHits[, hs := gene_id %in% disp[hs_decile == 10]$gene]
+bestHits[, RVIS := gene_id %in% disp[rvis_decile == 1]$gene]
+
+bestHits_melted <- melt(bestHits, id.vars = c("variant_id", "gene_id", "slope"), 
+                        measure.vars = c("pLI", "LOEUF", "pHaplo", "pTriplo", "hs", "RVIS"))
+
+pli_eqtl_p <- t.test(log(abs(bestHits[pLI == TRUE]$slope)), log(abs(bestHits[pLI == FALSE]$slope)))$p.value
+loeuf_eqtl_p <- t.test(log(abs(bestHits[LOEUF == TRUE]$slope)), log(abs(bestHits[LOEUF == FALSE]$slope)))$p.value
+pHaplo_eqtl_p <- t.test(log(abs(bestHits[pHaplo == TRUE]$slope)), log(abs(bestHits[pHaplo == FALSE]$slope)))$p.value
+pTriplo_eqtl_p <- t.test(log(abs(bestHits[pTriplo == TRUE]$slope)), log(abs(bestHits[pTriplo == FALSE]$slope)))$p.value
+hs_eqtl_p <- t.test(log(abs(bestHits[hs == TRUE]$slope)), log(abs(bestHits[hs == FALSE]$slope)))$p.value
+rvis_eqtl_p <- t.test(log(abs(bestHits[RVIS == TRUE]$slope)), log(abs(bestHits[RVIS == FALSE]$slope)))$p.value
+
+pval_eqtl_dt <- data.table(metric = c("1", "2", "3", "4", "5", "6"),
+                      pval = c(pli_eqtl_p, loeuf_eqtl_p, pHaplo_eqtl_p, pTriplo_eqtl_p, hs_eqtl_p, rvis_eqtl_p),
+                      y_facet = "Top 10%")
+
+eqtl_hist <- ggplot(data = bestHits_melted, aes(x = abs(slope), fill = value)) +
+  geom_histogram(size = 0.1, aes(y = after_stat(density))) +
+  facet_grid(value ~ variable) +
+  theme_bw() +
+  scale_fill_manual(values = c("#66c2a5", "#fc8d62")) +
+  ylab(expression("Absolute eQTL effect size (" ~ italic(hat(beta)) ~ ")"))
+
+eqtl_hist_dt <- layer_data(eqtl_hist) %>%
+  as.data.table() %>%
+  .[, metric := as.numeric(PANEL)]
+
+eqtl_hist_dt[metric > 6, metric := metric - 6]
+
+eqtl_hist_dt[, y_facet := as.character("NA")]
+eqtl_hist_dt[fill == "#fc8d62", y_facet := "Top 10%"]
+eqtl_hist_dt[fill == "#66c2a5", y_facet := "Lower 90%"]
+
+eqtl_hist_dt$y_facet <- factor(eqtl_hist_dt$y_facet, levels = c("Top 10%", "Lower 90%"))
+
+lower_panel <- ggplot(data = eqtl_hist_dt, aes(x = x, y = y, color = y_facet)) +
+  geom_point(size = 0.5) +
+  geom_line() +
+  facet_grid(. ~ metric) +
+  scale_color_brewer(palette = "Set2", name = "",
+                     guide = guide_legend(override.aes = list(color = "white"))) +
+  xlab(expression("Absolute eQTL effect size ( |" ~ italic(hat(beta)) ~ "| )")) +
+  ylab("Density") +
+  theme_minimal() +
+  xlim(-0.05, 2.6) +
+  theme(strip.text.x = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.text = element_text(color = "white"),
+        legend.title = element_text(color = "white"),
+        legend.key = element_rect(color = "white", fill = "white"))  +
+  geom_text(data = pval_eqtl_dt, aes(x = 1.7, y = 2.8, 
+                                label = paste("p =", formatC(pval, format = "e", digits = 2))), 
+            color = "black", size = 3)
+
+plot_grid(top_panel, lower_panel, nrow = 2, align = "v", axis = "lr")
