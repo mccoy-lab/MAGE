a081265a13789d701c7d0b6469859f32370dc587
mtassia <mtassia1@jhu.edu>
2026-04-10 15:30:10 -0400

    feat: expression_quantification jupyter notebook produces contrasts and expression matrices
    leafcutter R environment can now be succesfully created

---
+++ analysis_pipeline/01_data_preparation/splicing_quantification/02_intron_usage/workflow/bin/leafcutter_install.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+# Instructions from:
+# https://github.com/davidaknowles/leafcutter/issues/265#issuecomment-2679783289
+
+install.packages("BiocManager",
+                 dependencies = TRUE,
+                 repos = "https://cloud.r-project.org")
+BiocManager::install("Biobase",
+                     ask = FALSE,
+                     update = FALSE)
+install.packages("TailRank",
+                 dependencies = TRUE,
+                 repos = "https://cloud.r-project.org")
+BiocManager::install("DirichletMultinomial", 
+                     ask = FALSE,
+                     update = FALSE)
+devtools::install_github("davidaknowles/leafcutter/leafcutter",
+                         ref = "psi_2019",
+                         dependencies = TRUE,
+                         upgrade = "never")
